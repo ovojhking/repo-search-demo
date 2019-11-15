2495081cd865e02d14c9dd8d5eb6deb6bccfbfe3
jimmy <jhtokd@gmail.com>
2019-11-15 17:23:24 +0800

    feat: add InfiniteScroll and scroll detect library

---
+++ src/js/components/common/infiniteScroll/InfiniteScroll.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import { isScrollToBottom } from '../../../libs/scroll';
+
+class InfiniteScroll extends Component {
+	constructor() {
+		super();
+		this.lengthOfaddList = 10;
+		this.state = {
+			resourceList: [],
+			renderList: [],
+			isWait: false,
+		};
+	}
+
+	componentDidMount() {
+		window.addEventListener('scroll', this.handleScroll.bind(this));
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		const { resetFlag, resourceList } = this.props;
+		if (resetFlag) {
+			this.reset();
+		} else if (prevProps.resourceList.length === 0 && resourceList.length > 0) {
+			const stateResourceList = this.state.resourceList;
+			this.setState({ resourceList: [...stateResourceList, ...resourceList] }, () => {
+				this.pushRenderList();
+			});
+		} else if (!_.isEqual(resourceList, prevProps.resourceList)) {
+			const stateResourceList = this.state.resourceList;
+			this.setState({ resourceList: [...stateResourceList, ...resourceList] });
+		}
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.handleScroll.bind(this));
+	}
+
+	setDelay(time, callBack) {
+		this.setState({ isWait: true });
+		setTimeout(
+			() => {
+				this.setState({ isWait: false });
+				callBack();
+			},
+			time,
+		);
+	}
+
+	setLoader() {
+		const { isWait } = this.state;
+		const { loader } = this.props;
+		if (isWait) {
+			return loader();
+		}
+		return '';
+	}
+
+	setRenderList() {
+		const { resourceList } = this.state;
+		const { updateResourceList, renderItem } = this.props;
+
+		if (resourceList.length <= this.lengthOfaddList * 2) {
+			console.log('time to update!');
+			updateResourceList();
+		}
+
+		const renderList = [];
+		let counter = 0;
+		while (counter < this.lengthOfaddList && resourceList.length > 0) {
+			counter += 1;
+			const firstItem = resourceList.shift();
+			renderList.push(
+				renderItem(firstItem),
+			);
+		}
+
+		this.setState({ resourceList });
+		return renderList;
+	}
+
+	reset() {
+		const { setResetFlag } = this.props;
+		this.setState({
+			resourceList: [],
+			renderList: [],
+			isWait: false,
+		});
+
+		setResetFlag();
+	}
+
+	handleScroll() {
+		if (isScrollToBottom() && !this.state.isWait) {
+			const { resourceList } = this.props;
+			if (resourceList.length !== 0) {
+				const delayTime = this.props.loadingDelay ? this.props.loadingDelay : 0;
+				this.setDelay(delayTime, this.pushRenderList.bind(this));
+			}
+		}
+	}
+
+	pushRenderList() {
+		const { renderList } = this.state;
+		renderList.push(
+			this.setRenderList(),
+		);
+		this.setState({ renderList });
+	}
+
+	render() {
+		const { renderList } = this.state;
+		return (
+			<div>
+				<div>
+					{ renderList }
+				</div>
+
+				{ this.setLoader() }
+			</div>
+		);
+	}
+}
+
+export default InfiniteScroll;
---
+++ src/js/libs/scroll.js
@@ -0,0 +1,52 @@
+function getScrollTop() {
+	let scrollTop = 0;
+	let bodyScrollTop = 0;
+	let documentScrollTop = 0;
+	if (document.body) {
+		bodyScrollTop = document.body.scrollTop;
+	}
+	if (document.documentElement) {
+		documentScrollTop = document.documentElement.scrollTop;
+	}
+	scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+	return scrollTop;
+}
+
+function getScrollHeight() {
+	let scrollHeight = 0;
+	let bodyScrollHeight = 0;
+	let documentScrollHeight = 0;
+	if (document.body) {
+		bodyScrollHeight = document.body.scrollHeight;
+	}
+	if (document.documentElement) {
+		documentScrollHeight = document.documentElement.scrollHeight;
+	}
+	scrollHeight = (bodyScrollHeight - documentScrollHeight > 0)
+		? bodyScrollHeight : documentScrollHeight;
+	return scrollHeight;
+}
+
+function getWindowHeight() {
+	let windowHeight = 0;
+	if (document.compatMode === 'CSS1Compat') {
+		windowHeight = document.documentElement.clientHeight;
+	} else {
+		windowHeight = document.body.clientHeight;
+	}
+	return windowHeight;
+}
+
+function isScrollToBottom() {
+	if (getScrollTop() + getWindowHeight() === getScrollHeight()) {
+		return true;
+	}
+	return false;
+}
+
+export {
+	getScrollTop,
+	getScrollHeight,
+	getWindowHeight,
+	isScrollToBottom,
+};
