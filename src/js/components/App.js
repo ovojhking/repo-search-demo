245d92940202b1b86021dbd7aab6c6f97a7ff09e
jimmy <jhtokd@gmail.com>
2019-11-15 16:26:15 +0800

    feat: project init

---
+++ src/js/components/App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import '../../styles/sass/app.scss';
+import Main from './contents/Main/Main';
+
+const App = () => <Main />;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/js/index.js',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'index-bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: [
+					{ loader: 'babel-loader' },
+				],
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(scss|sass)$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'sass-loader',
+					},
+				],
+			},
+			{
+				test: /\.(png|jp(e*)g|svg)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 8000, // Convert images < 8kb to base64 strings
+						name: 'images/[hash]-[name].[ext]',
+					},
+				}],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+	],
+};
