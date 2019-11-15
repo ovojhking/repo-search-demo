001b2946abff5a9ba1c8c48d6ba57bf5816ce81b
jimmy <jhtokd@gmail.com>
2019-11-15 17:37:42 +0800

    feat: add DataFormat to library

---
+++ src/js/libs/DateFormat.js
@@ -0,0 +1,21 @@
+const DateFormat = class DateFormat {
+	constructor() {
+		this.dateFormated;
+		this.monthsInEnglish = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	}
+
+	init(date) {
+		const dateFormated = new Date(date);
+		this.dateFormated = dateFormated;
+	}
+
+	getAmericanFormat() {
+		return `${this.monthsInEnglish[this.dateFormated.getMonth()]} ${this.dateFormated.getDate()} ${this.dateFormated.getFullYear()}`;
+	}
+
+	getBritishFormat() {
+		return `${this.dateFormated.getDate()} ${this.monthsInEnglish[this.dateFormated.getMonth()]} ${this.dateFormated.getFullYear()}`;
+	}
+};
+
+export default DateFormat;
