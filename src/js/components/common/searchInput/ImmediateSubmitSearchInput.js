fe6719d9153045e8b6f95232b79139b1b1db037d
jimmy <jhtokd@gmail.com>
2019-11-15 16:39:02 +0800

    feat: add ImmediateSubmitSearchInput component and css

---
+++ src/js/components/common/searchInput/ImmediateSubmitSearchInput.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class ImmediateSubmitSearchInput extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchText: props.defaultValue ? props.defaultValue : '',
+		};
+		this.onSearchTextChange = this.onSearchTextChange.bind(this);
+	}
+
+	onSearchTextChange(event) {
+		const searchText = event.target.value;
+		this.setState({ searchText });
+		this.props.getSearchText(searchText);
+	}
+
+	render() {
+		const { searchText } = this.state;
+		return (
+			<div className="position-relative">
+				<i className="position-absolute search-icon fas fa-search" />
+				<input className="search-input border border-primary-dark rounded" type="text" value={searchText} placeholder="請輸入文字" onChange={this.onSearchTextChange} />
+			</div>
+		);
+	}
+}
+
+export default ImmediateSubmitSearchInput;
