ac664342677d10e0702cb52b25093c49ae07c984
jimmy <jhtokd@gmail.com>
2019-11-16 18:27:58 +0800

    refactor: change main and header into pure component

--- src/js/components/common/partials/Header.js
+++ src/js/components/common/partials/Header.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import RepoSearchInput from '../repoSearch/RepoSearchInput';
 import Logo from '../../../../assets/images/logo.png';
 
-class Header extends Component {
-	render() {
-		return (
-			<div className="header d-flex justify-content-center align-items-center">
-				<div className="col-12 col-xl-10 h-100 d-flex justify-content-center align-items-center">
-					<div className="d-none d-sm-block col-sm-2 col-lg-1 h-70">
-						<img className="logo h-100" src={Logo} alt="logo" />
-					</div>
-					<div className="col-8 col-sm-8 col-lg-9">
-						<RepoSearchInput />
-					</div>
-
-					<a className="col-4 col-sm-2 col-lg-2 text-white" href="https://www.cakeresume.com/s--7hIWbrdfMy9nVV0UQ_I9cA--/jimmy-eab4dd">
-						<i className="fa-lg fas fa-id-card" />
-						<span className="ml-2">履歷</span>
-					</a>
-				</div>
+const Header = () => (
+	<div className="header d-flex justify-content-center align-items-center">
+		<div className="col-12 col-xl-10 h-100 d-flex justify-content-center align-items-center">
+			<div className="d-none d-sm-block col-sm-2 col-lg-1 h-70">
+				<img className="logo h-100" src={Logo} alt="logo" />
 			</div>
-		);
-	}
-}
+			<div className="col-8 col-sm-8 col-lg-9">
+				<RepoSearchInput />
+			</div>
+			<a className="col-4 col-sm-2 col-lg-2 text-white" href="https://www.cakeresume.com/s--7hIWbrdfMy9nVV0UQ_I9cA--/jimmy-eab4dd">
+				<i className="fa-lg fas fa-id-card" />
+				<span className="ml-2">履歷</span>
+			</a>
+		</div>
+	</div>
+);
 
 export default Header;
--- src/js/components/contents/Main/Main.js
+++ src/js/components/contents/Main/Main.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from '../../common/partials/Header';
 import RepoSearchContent from '../../common/repoSearch/RepoSearchContent';
 
-class Main extends Component {
-	render() {
-		return (
-			<div>
-				<Header />
-				<div className="container-fix-top d-flex justify-content-center">
-					<div className="col-12 col-lg-10 col-xl-10 p-4">
-						<RepoSearchContent />
-					</div>
-				</div>
+const Main = () => (
+	<div>
+		<Header />
+		<div className="container-fix-top d-flex justify-content-center">
+			<div className="col-12 col-lg-10 col-xl-10 p-4">
+				<RepoSearchContent />
 			</div>
-		);
-	}
-}
+		</div>
+	</div>
+);
 
 export default Main;
